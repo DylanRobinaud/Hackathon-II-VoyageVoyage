7a19364726ff587f1d660c01300e8cac9b8539fb
nicolas follain <n.follain@yahoo.fr>
2024-08-13 12:09:40 +0200

    mise en place du component card jsx et css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import logo from "./assets/images/zarla-voyage-voyage.png";
-import Card from "./components/card/Card";
+
 import "./App.css";
 
 function App() {
   return (
     <main>
-      <Card town="New York" subtitle="Etats Unis" photoUrl={logo} />
+      <h1>Voyage Voyage</h1>
+      <img src={logo} alt="logo" />
     </main>
   );
 }
