3fa5602f88039a9cbac207ba2755da8194ca92db
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-08-13 14:08:47 +0200

    remettre app comme avant

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
-// import Welcome from "./pages/Welcome/Welcome";
-import Test from "./components/searchSection/SearchSection";
+import Welcome from "./pages/Welcome/Welcome";
 
 import "./App.css";
 
 function App() {
   return (
     <main>
-      <Test />
+      <Welcome />
     </main>
   );
 }
