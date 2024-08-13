d3293f59bf928cc6326990c0a82da265f4d6bea6
nicolas follain <n.follain@yahoo.fr>
2024-08-13 21:54:36 +0200

    mise en place resultcard

---
+++ client/src/pages/FormPage/resultat/ResultatPage.jsx
@@ -0,0 +1,16 @@
+import ResultCard from "../../../components/resultCard/Resultcard";
+import H2Component from "../../../components/ui/text/H2component";
+import H3Component from "../../../components/ui/text/H3component";
+
+function Resultat() {
+  return (
+    <>
+      <div>resultat</div>
+      <ResultCard />
+      <H2Component />
+      <H3Component />
+    </>
+  );
+}
+
+export default Resultat;
